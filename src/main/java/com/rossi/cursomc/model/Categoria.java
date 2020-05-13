d9c68b3f02d88a3d9a2b14d08b51a955f31a1e7e
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-05-12 23:24:39 -0300

    Banco de dados H2 e criação automática da base de dados apartir dos objetos

--- src/main/java/com/rossi/cursomc/model/Categoria.java
+++ src/main/java/com/rossi/cursomc/model/Categoria.java
@@ -2,6 +2,11 @@ package com.rossi.cursomc.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +18,15 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
+
+@Entity // Esta anotação que faz com que a tabela seja criada no banco de dados
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
     private String nome;
 }
