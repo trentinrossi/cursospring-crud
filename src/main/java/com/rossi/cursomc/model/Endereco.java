92605b4d75a395c1693ebbbd01a190089b138bba
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-05-14 21:48:02 -0300

    Classe cliente e endereço

---
+++ src/main/java/com/rossi/cursomc/model/Endereco.java
@@ -0,0 +1,30 @@
+package com.rossi.cursomc.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Endereco implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+    private Cliente cliente;
+
+    private Cidade cidade;
+}
